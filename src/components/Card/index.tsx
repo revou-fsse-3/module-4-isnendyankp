54e515d62569fa906080180a94a28a8b06a66bba
isnendyankp <isnendyankp@gmail.com>
2023-12-22 10:58:37 +0700

    add condition to className with border props value true and add tailwind css

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 const Card = ({ border } : Props) => {
     return (
-        <div>
+        <div className={`${border && 'rounded border-slate-600 border'}`}>
             
         </div>
     );
@@ -19,3 +19,4 @@ export default Card;
 // 1. cr8 Card component
 // 2. cr8 interface Props has border props with boolean type
 // 3. add border props as a parameter to Card component with type Props
+// 4. add condition to className with border props value true and add rounded and border slate 600 border to className value 
