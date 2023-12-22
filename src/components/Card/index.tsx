7dca616940f9b12e3c9d78948f1f0e7d4578d72c
isnendyankp <isnendyankp@gmail.com>
2023-12-22 10:46:47 +0700

    cr8 interface Props has border props with boolean type

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,5 +1,10 @@
 
 
+interface Props {
+    border: boolean;
+}
+
+
 const Card = () => {
     return (
         <div>
@@ -12,3 +17,4 @@ export default Card;
 
 // Progress:
 // 1. cr8 Card component
+// 2. cr8 interface Props has border props with boolean type
