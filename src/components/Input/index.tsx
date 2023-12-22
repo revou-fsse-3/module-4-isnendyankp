7eda95cfe0810ac157989f838fd334802e4fe8b1
isnendyankp <isnendyankp@gmail.com>
2023-12-22 13:49:09 +0700

    add spread props to input element

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -8,7 +8,7 @@ const Input = (props : InputProps) => {
     <input
       type="text"
       {...props}
-      className="block flex-1 border-black bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+      className={`${props.className} block flex-1 border-2 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6`}
     />
   );
 }
@@ -19,4 +19,5 @@ export default Input
 //  1. cr8 input component
 //  2. Add type InputProps with InputHTMLAttributes<HTMLInputElement> from react
 //  3. Add props with type InputProps to Input component and spread it to input element
-//  4. styling input element with tailwind className props  
+//  4. styling input element with tailwind className props
+//  5. add spread props to input element 
