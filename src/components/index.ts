dc6621000441c6801e698ebeead10effee7e5838
isnendyankp <isnendyankp@gmail.com>
2023-12-22 11:17:59 +0700

    export card component for file distributor

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,8 +3,10 @@
 export { default as Input } from './Input';
 export { default as Text } from './Text';
 export { default as Button } from './Button';
+export { default as Card } from './Card';
 
 // Progress:
 // 1. export input component for file distributor
 // 2. export text component for file distributor
 // 3. export button component for file distributor
+// 4. export card component for file distributor
