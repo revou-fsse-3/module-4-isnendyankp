7173ccee189daf0336e22d0f1e3eced7cd982c2a
isnendyankp <isnendyankp@gmail.com>
2024-01-04 16:56:02 +0700

    remove junk code

--- src/containers/LoginContainer/index.tsx
+++ src/containers/LoginContainer/index.tsx
@@ -64,20 +64,3 @@ const LoginPage = () => {
 
 export default LoginPage;
 
-
-
-// const LoginContainer = () => {
-//   return (
-//     <div>
-//       <h1>Halaman Login</h1>
-//     </div>
-//   );
-// };
-
-// export default LoginContainer;
-
-
-// Progress:
-// 1. cr8 AboutContainer component
-// 2. import Navbar & render @AboutContainer component
-// 3. remove Navbar cz already use nested route with element PublicLayout component
