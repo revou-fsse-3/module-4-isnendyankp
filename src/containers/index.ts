8f729d7eeac9bd9d8d5ea7ca31f06713c81a035c
isnendyankp <isnendyankp@gmail.com>
2024-01-07 18:40:44 +0700

    Add CreateCategoryContainer file Distributor

--- src/containers/index.ts
+++ src/containers/index.ts
@@ -1,10 +1,4 @@
 export { default as HomeContainer } from './HomeContainer';
 export { default as LoginContainer } from './LoginContainer';
 export { default as ListContainer } from './ListContainer';
-
-
-// Progress:
-// 1. Create file distributor src/containers/index.ts
-// 2. Add HomeContainer for container file distributor  @src/containers/index.ts
-// 3. Add LoginContainer for container file distributor  @src/containers/index.ts
-// 4. Add ListContainer for container file distributor  @src/containers/index.ts
+export { default as CreateCategoryContainer } from './CreateCategoryContainer';
