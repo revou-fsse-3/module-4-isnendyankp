aeb958dbcd404bb0101f6177af6cb3820abf6f98
isnendyankp <isnendyankp@gmail.com>
2024-01-21 07:46:43 +0700

    add styling navbar move to top page

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -4,9 +4,9 @@ const Navbar = () => {
   const navigate = useNavigate();
 
   return (
-    <nav className="bg-gray-800">
+    <nav className="bg-gray-800 fixed w-full top-0 left-0 z-50">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-        <div className="relative flex h-16 items-center justify-between">
+        <div className="relative flex items-center justify-between h-16">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
             {/* <!-- Mobile menu button--> */}
             <button
